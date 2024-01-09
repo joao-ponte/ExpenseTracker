dfd9561aee0e41497448de26f3360f2debbb3a8a
Joao Ponte <joao_ponte@msn.com>
2024-01-09 10:39:27 +0000

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by João Ponte on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
